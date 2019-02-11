6d34ea258a0c04a94b6cec5abb6a16faf0206086
Jason (제이슨) <jasonjerome16@gmail.com>
2019-02-10 21:51:33 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+server_ip = "SERVER_IP"
+user_id = "BOT_USER"
+password = "BOT_PASSWORD"
+port = PORT_NUMBER
+user_cert = "BOT_CERT_PATH"
+
+temporary_img_dir = "TEMP_IMAGE_DIR"
+temporary_media_dir = "TEMP_MEDIA_DIR"
+permanent_media_dir = "PERM_MEDIA_DIR"
+
+vlc_dir = "VLC_DIR"
+default_channel = "DEFAULT_CHANNEL"
+plugins_path = "PLUGINS_PATH"
+privileges_path = "PRIV_FILE_PATH"
+
+
+
+bot_version = "v2.2.3_p2"
+about_text = "<br> DuckBot is a plugin-based mumble bot client. <br> DuckBot is currently on version: %s" % bot_version
+
+known_bugs = "<br>#### Known Bugs ####<br>\
+            <b></b><br>\
+            <br>"
