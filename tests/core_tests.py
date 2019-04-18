f779bd8c31c86c2bc1adcb446b848d1fc72546cf
Jason <jay-duck@naver.com>
2019-04-18 14:20:31 -0400

    Update core_tests.py

--- tests/core_tests.py
+++ tests/core_tests.py
@@ -4,7 +4,7 @@ import utils
 
 
 class Tests(unittest.TestCase):
-	def test_version(self):
-		# Initialize configs.
+    def test_version(self):
+        # Initialize configs.
         GM.cfg.read(utils.get_tests_dir())
         self.assertEqual(GM.cfg['Bot_Information']['BotVersion'], "v1.8.2")
