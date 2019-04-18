3b30d359a7deb05ca7e993799aac9498b2c397f1
Jason <jay-duck@naver.com>
2019-04-18 14:54:38 -0400

    Travis CI Updates

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,7 +7,12 @@ from helpers.global_access import GlobalMods as GM
 
 
 class Tests(unittest.TestCase):
-    def test_version(self):
+    def setUp(self):
         # Initialize configs.
         GM.cfg.read(f"{utils.get_tests_dir()}/sample_config.ini")
+    def test_version(self):
         self.assertEqual(GM.cfg['Bot_Information']['BotVersion'], "v1.8.2")
+
+
+if __name__ == '__main__':
+	unittest.main()
