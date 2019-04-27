c036f473a8fed2f3670a6f935522a74ea6faf80b
Jason <jay-duck@naver.com>
2019-04-26 22:50:42 -0400

    Updated GlobalMods to use a single reference mumble instance
    - Moved uptime methods/calculations to global_access

--- helpers/global_access.py
+++ helpers/global_access.py
@@ -1,7 +1,10 @@
 import configparser
+import time
 
 
 class GlobalMods:
+    # Mumble instance
+    mumble = None
     # Config Access
     cfg = configparser.ConfigParser()
     # PGUI System Access
@@ -15,6 +18,12 @@ class GlobalMods:
     safe_mode = False
     verbose_mode = False
     quiet_mode = False
+    # Up-time Tracker
+    start_seconds = None
+    seconds = 0
+    minutes = 0
+    hours = 0
+    days = 0
 
 
 def debug_print(msg):
@@ -25,3 +34,17 @@ def debug_print(msg):
 def reg_print(msg):
     if not GlobalMods.quiet_mode:
         print(msg)
+
+
+def check_time():
+    GlobalMods.seconds = time.time() - GlobalMods.start_seconds
+    while GlobalMods.seconds >= 60:
+        GlobalMods.minutes += 1
+        GlobalMods.seconds -= 60
+    while GlobalMods.minutes >= 60:
+        GlobalMods.hours += 1
+        GlobalMods.minutes -= 60
+    while GlobalMods.hours >= 24:
+        GlobalMods.days += 1
+        GlobalMods.hours -= 24
+    return "Up-time: {%dd : %dh : %dm : %ds}" % (int(GlobalMods.days), int(GlobalMods.hours), int(GlobalMods.minutes), int(GlobalMods.seconds))
