94b8f2937a1008b6341375004efedf8fb5b62dfe
Jason (제이슨) <jasonjerome16@gmail.com>
2019-08-26 19:45:03 +0530

    Added static string resources

---
+++ JJMumbleBot/lib/resources/strings.py
@@ -0,0 +1,73 @@
+###########################################################################
+# PROGRAM CONFIG CATEGORY STRINGS
+C_CONNECTION_SETTINGS = 'Connection Settings'
+C_MAIN_SETTINGS = 'Main Settings'
+C_PLUGIN_SETTINGS = 'Plugin Settings'
+C_PGUI_SETTINGS = 'PGUI Settings'
+C_MEDIA_DIR = 'Media Directories'
+C_WEB_INT = 'Web Interface'
+C_LOGGING = 'Logging'
+
+# PROGRAM CONFIG PARAMETER STRINGS
+# Connection Settings
+P_SERVER_IP = 'ServerIP'
+P_SERVER_PASS = 'ServerPassword'
+P_SERVER_PORT = 'ServerPort'
+P_USER_ID = 'UserID'
+P_USER_CERT = 'UserCertification'
+P_CHANNEL_DEF = 'DefaultChannel'
+# Main Settings
+P_CMD_TICK_RATE = 'CommandTickRate'
+P_CMD_MULTI_LIM = 'MultiCommandLimit'
+P_CMD_TOKEN = 'CommandToken'
+P_CMD_QUEUE_LIM = 'CommandQueueLimit'
+P_CMD_HIST_LIM = 'CommandHistoryLimit'
+# Permanent Directories
+P_PERM_MEDIA_DIR = 'PermanentMediaDirectory'
+# Temporary Directories
+P_TEMP_IMG_DIR = 'TemporaryImageDirectory'
+# PGUI Settings
+P_CANVAS_BG_COL = 'CanvasBGColor'
+P_CANVAS_ALGN = 'CanvasAlignment'
+P_CANVAS_BORD = 'CanvasBorder'
+P_CANVAS_TXT_COL = 'CanvasTextColor'
+P_TXT_HEAD_COL = 'HeaderTextColor'
+P_TXT_IND_COL = 'IndexTextColor'
+# Plugin Settings
+P_PLUG_DISABLED = 'DisabledPlugins'
+# Web Interface Settings
+P_WEB_ENABLE = 'EnableWebInterface'
+P_WEB_IP = 'WebInterfaceIP'
+P_WEB_PORT = 'WebInterfacePort'
+# Logging
+P_LOG_ENABLE = 'EnableLogging'
+P_LOG_MAX = 'MaxLogs'
+P_LOG_DIR = 'LogDirectory'
+###########################################################################
+# LOGGING STRINGS
+INFO = 'info'
+DEBUG = 'debug'
+WARNING = 'warning'
+CRITICAL = 'critical'
+###########################################################################
+# PLUGIN CONFIG CATEGORY STRINGS
+C_PLUGIN_INFO = 'Plugin Information'
+C_PLUGIN_REQS = 'Plugin Requirements'
+C_PLUGIN_TYPE = 'Plugin Type'
+C_PLUGIN_SET = 'Plugin Settings'
+# PLUGIN CONFIG PARAMETER STRINGS
+# Plugin Information
+P_PLUGIN_VERS = 'PluginVersion'
+P_PLUGIN_NAME = 'PluginName'
+P_PLUGIN_DESC = 'PluginDescription'
+P_PLUGIN_CMDS = 'PluginCommands'
+P_PLUGIN_LANG = 'PluginLanguage'
+# Plugin Type
+P_AUD_PLUGIN = 'AudioPlugin'
+P_IMG_PLUGIN = 'ImagePlugin'
+P_CORE_PLUGIN = 'CorePlugin'
+P_EXT_PLUGIN = 'ExtensionPlugin'
+# Universal Plugin Settings
+P_THREAD_WAIT = 'ThreadWaitForCommands'
+P_THREAD_SINGLE = 'UseSingleThread'
+###########################################################################
