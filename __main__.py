4fab547342b33c75b0674d1f8bbbc105fe065c6a
Jason (제이슨) <jasonjerome16@gmail.com>
2019-08-26 19:26:50 +0530

    Cleared repo for v3.0.0 rework

--- __main__.py
+++
@@ -1,4 +0,0 @@
-import JJMumbleBot
-if __name__ == "__main__":
-    print("JJ Mumble Bot Booting...")
-    JJMumbleBot.JJMumbleBot()
--- helpers/command.py
+++
@@ -1,8 +0,0 @@
-class Command:
-
-    command = None
-    text = None
-
-    def __init__(self, command, text):
-        self.command = command
-        self.text = text
