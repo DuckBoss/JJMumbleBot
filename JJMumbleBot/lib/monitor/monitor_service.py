f5ceccf618b24c323172723d3257d25ff499464b
DuckBoss <jasonjerome16@gmail.com>
2019-08-30 16:06:16 +0530

    Removed unused imports/code

--- JJMumbleBot/lib/monitor/monitor_service.py
+++ JJMumbleBot/lib/monitor/monitor_service.py
@@ -1,10 +1,6 @@
 from JJMumbleBot.lib.monitor import monitor_service_utility as msu
-from JJMumbleBot.settings import runtime_settings
-import time
 
 
-def loop_monitor_check():
+def monitor_check():
     msu.check_cpu_percent()
     msu.check_ram_percent()
-    # time.sleep(runtime_settings.tick_rate)
-
