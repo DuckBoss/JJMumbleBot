1e436f0b9dca51022a6681521792c98318f6183b
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-03 23:33:44 -0400

    Removed web interface monitoring tests

--- JJMumbleBot/lib/monitor/monitor_data.py
+++
@@ -1,5 +0,0 @@
-# System Information
-cpu_percent = '0'
-ram_percent = '0'
-# Mumble Information
-online_users = []
--- JJMumbleBot/lib/monitor/monitor_service.py
+++
@@ -1,6 +0,0 @@
-from JJMumbleBot.lib.monitor import monitor_service_utility as msu
-
-
-def monitor_check():
-    msu.check_cpu_percent()
-    msu.check_ram_percent()
