2874be0210fae718ea1ff23530bec62f506831c3
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-07 13:34:49 -0400

    Converted unit tests to pytest

--- JJMumbleBot/tests/test_cfg.py
+++ JJMumbleBot/tests/test_cfg.py
@@ -3,53 +3,50 @@ from JJMumbleBot.lib.utils.runtime_utils import get_version
 from JJMumbleBot.settings import global_settings
 from JJMumbleBot.lib.resources.strings import *
 import configparser
-import unittest
+
+
 # sys.path.append(".")
 # sys.path.append(".")
 
 
-class Tests(unittest.TestCase):
-    def setUp(self):
+class Test_Cfg:
+    def setup_method(self):
         # Initialize configs.
         global_settings.cfg = configparser.ConfigParser()
         global_settings.cfg.read(f"{get_main_dir()}/cfg/templates/config_template.ini")
 
     def test_version(self):
         bot_version = get_version()
-        self.assertEqual(bot_version, META_VERSION)
+        assert bot_version == META_VERSION
 
     def test_server_ip(self):
         server_ip = global_settings.cfg[C_CONNECTION_SETTINGS][P_SERVER_IP]
-        self.assertEqual(server_ip, "SERVER_IP")
+        assert server_ip == "SERVER_IP"
 
     def test_user_id(self):
         user_id = global_settings.cfg[C_CONNECTION_SETTINGS][P_USER_ID]
-        self.assertEqual(user_id, "USERNAME")
+        assert user_id == "USERNAME"
 
     def test_server_pass(self):
         server_pass = global_settings.cfg[C_CONNECTION_SETTINGS][P_SERVER_PASS]
-        self.assertEqual(server_pass, "PASSWORD")
+        assert server_pass == "PASSWORD"
 
     def test_server_port(self):
         server_port = global_settings.cfg[C_CONNECTION_SETTINGS][P_SERVER_PORT]
-        self.assertEqual(server_port, "PORT_NUMBER")
+        assert server_port == "PORT_NUMBER"
 
     def test_user_cert(self):
         user_cert = global_settings.cfg[C_CONNECTION_SETTINGS][P_USER_CERT]
-        self.assertEqual(user_cert, "CERT_FILE_PATH")
+        assert user_cert == "CERT_FILE_PATH"
 
     def test_def_channel(self):
         default_channel = global_settings.cfg[C_CONNECTION_SETTINGS][P_CHANNEL_DEF]
-        self.assertEqual(default_channel, "DEFAULT_CHANNEL_NAME")
+        assert default_channel == "DEFAULT_CHANNEL_NAME"
 
     def test_super_user(self):
         super_user = global_settings.cfg[C_CONNECTION_SETTINGS][P_DEFAULT_SU]
-        self.assertEqual(super_user, "DEFAULT_SUPER_USER_NAME")
+        assert super_user == "DEFAULT_SUPER_USER_NAME"
 
     def test_self_register(self):
         self_register = global_settings.cfg.getboolean(C_CONNECTION_SETTINGS, P_SELF_REGISTER)
-        self.assertEqual(self_register, True)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        assert self_register is True
--- JJMumbleBot/tests/test_conn.py
+++ JJMumbleBot/tests/test_conn.py
@@ -3,48 +3,40 @@ import pymumble_py3 as pymumble
 from JJMumbleBot.lib.helpers.bot_service_helper import BotServiceHelper
 from JJMumbleBot.settings import global_settings
 import configparser
-import unittest
 
 
 # sys.path.append(".")
 # sys.path.append(".")
 
 
-class Tests(unittest.TestCase):
-    md = None
-
-    def setUp(self):
-        global md
+class Test_Conn:
+    def setup_method(self):
         # Initialize configs.
         global_settings.cfg = configparser.ConfigParser()
         global_settings.cfg.read(f"{get_main_dir()}/cfg/templates/config_template.ini")
-        md = BotServiceHelper.retrieve_mumble_data()
+        self.md = BotServiceHelper.retrieve_mumble_data()
 
     def test_connectivity(self):
         mumble_inst = pymumble.Mumble('127.0.0.1', port=64738, user='TravisCIClient',
                                       password='test')
-        self.assertEqual(mumble_inst, not None)
+        assert mumble_inst is not None
 
     def test_server_ip(self):
-        server_ip = md.server_ip
-        self.assertEqual(server_ip, "SERVER_IP")
+        server_ip = self.md.server_ip
+        assert server_ip == "SERVER_IP"
 
     def test_user_id(self):
-        user_id = md.user_id
-        self.assertEqual(user_id, "USERNAME")
+        user_id = self.md.user_id
+        assert user_id == "USERNAME"
 
     def test_server_pass(self):
-        server_pass = md.server_pass
-        self.assertEqual(server_pass, "PASSWORD")
+        server_pass = self.md.server_pass
+        assert server_pass == "PASSWORD"
 
     def test_server_port(self):
-        server_port = md.server_port
-        self.assertEqual(server_port, "PORT_NUMBER")
+        server_port = self.md.server_port
+        assert server_port == "PORT_NUMBER"
 
     def test_user_cert(self):
-        user_cert = md.user_cert
-        self.assertEqual(user_cert, "CERT_FILE_PATH")
-
-
-if __name__ == '__main__':
-    unittest.main()
+        user_cert = self.md.user_cert
+        assert user_cert == "CERT_FILE_PATH"
