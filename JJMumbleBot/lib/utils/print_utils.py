9de297fcfbad6b078c27cb19a6ad48a7380236e3
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-07 03:24:54 -0400

    Updated print_utils to accomodate origin-specific info

--- JJMumbleBot/lib/utils/print_utils.py
+++ JJMumbleBot/lib/utils/print_utils.py
@@ -1,11 +1,12 @@
 import JJMumbleBot.settings.global_settings as GS
+from JJMumbleBot.lib.resources.strings import *
 
 
-def dprint(msg):
+def dprint(msg, origin=None):
     if GS.verbose_mode and not GS.quiet_mode:
-        print(msg)
+        print(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{msg}')
 
 
-def rprint(msg):
+def rprint(msg, origin=None):
     if not GS.quiet_mode:
-        print(msg)
+        print(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{msg}')
