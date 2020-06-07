a2d9ecbefcb004773694a8745d1141a6d07e485d
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-07 14:19:40 -0400

    Updated unit tests to use dummy configs

--- JJMumbleBot/tests/test_conn.py
+++ JJMumbleBot/tests/test_conn.py
@@ -12,30 +12,30 @@ class Test_Conn:
     def setup_method(self):
         # Initialize configs.
         global_settings.cfg = configparser.ConfigParser()
-        global_settings.cfg.read(f"{get_main_dir()}/cfg/templates/config_template.ini")
+        global_settings.cfg.read(f"{get_main_dir()}/tests/dummy_config.ini")
         self.md = BotServiceHelper.retrieve_mumble_data()
 
     def test_connectivity(self):
-        mumble_inst = pymumble.Mumble('127.0.0.1', port=64738, user='TravisCIClient',
-                                      password='test')
+        mumble_inst = pymumble.Mumble(self.md.ip_address, port=self.md.port, user=self.md.user_id,
+                                      password=self.md.password)
         assert mumble_inst is not None
 
     def test_server_ip(self):
         server_ip = self.md.server_ip
-        assert server_ip == "SERVER_IP"
+        assert server_ip == "127.0.0.1"
 
     def test_user_id(self):
         user_id = self.md.user_id
-        assert user_id == "USERNAME"
+        assert user_id == "TravisCIClient"
 
     def test_server_pass(self):
         server_pass = self.md.server_pass
-        assert server_pass == "PASSWORD"
+        assert server_pass == "test"
 
     def test_server_port(self):
         server_port = self.md.server_port
-        assert server_port == "PORT_NUMBER"
+        assert server_port == 64738
 
     def test_user_cert(self):
         user_cert = self.md.user_cert
-        assert user_cert == "CERT_FILE_PATH"
+        assert user_cert == "test_cert"
