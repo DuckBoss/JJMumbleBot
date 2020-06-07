8daa7800ec08ac8e05c8e3c457967ef04c64ea96
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-07 03:30:25 -0400

    Updated auto-updater plugin to new systems

--- JJMumbleBot/plugins/core/auto_updater/auto_updater.py
+++ JJMumbleBot/plugins/core/auto_updater/auto_updater.py
@@ -3,6 +3,7 @@ from JJMumbleBot.settings import global_settings as GS
 from JJMumbleBot.lib import privileges
 from JJMumbleBot.lib.utils.print_utils import rprint, dprint
 from JJMumbleBot.lib.utils.plugin_utils import PluginUtilityService
+from JJMumbleBot.lib.helpers.bot_service_helper import log
 from JJMumbleBot.lib.resources.strings import *
 from JJMumbleBot.plugins.core.auto_updater.utility import auto_updater_helper as update_utils
 
@@ -12,22 +13,26 @@ class Plugin(PluginBase):
         super().__init__()
         import os
         import json
-        raw_file = os.path.basename(__file__)
-        self.metadata = PluginUtilityService.process_metadata(f'plugins/core/{raw_file}')
+        self.plugin_name = os.path.basename(__file__).rsplit('.')[0]
+        self.metadata = PluginUtilityService.process_metadata(f'plugins/core/{self.plugin_name}')
         self.plugin_cmds = json.loads(self.metadata.get(C_PLUGIN_INFO, P_PLUGIN_CMDS))
-        self.priv_path = f'plugins/core/{raw_file.split(".")[0]}/privileges.csv'
-        self.help_path = f'plugins/core/{raw_file.split(".")[0]}/help.html'
         rprint(
             f"{self.metadata[C_PLUGIN_INFO][P_PLUGIN_NAME]} v{self.metadata[C_PLUGIN_INFO][P_PLUGIN_VERS]} Plugin Initialized.")
+
+    def quit(self):
+        dprint(f"Exiting {self.plugin_name} plugin...")
+        log(INFO, f"Exiting {self.plugin_name} plugin...", origin=L_SHUTDOWN)
+
+    def get_metadata(self):
+        return self.metadata
 
     def process(self, text):
         message = text.message.strip()
         message_parse = message[1:].split(' ', 1)
-        all_messages = message[1:].split()
         command = message_parse[0]
 
         if command == "update":
-            if not privileges.plugin_privilege_checker(text, command, self.priv_path):
+            if not privileges.plugin_privilege_checker(text, command, self.plugin_name):
                 return
             res = update_utils.update_available(message_parse[1])
             if res is True:
@@ -47,10 +52,9 @@ class Plugin(PluginBase):
             else:
                 GS.gui_service.quick_gui(f"There is no update available for: [{message_parse[1]}].",
                                          text_type='header', box_align='left', ignore_whisper=True)
-            return
 
-        if command == "checkforupdates":
-            if not privileges.plugin_privilege_checker(text, command, self.priv_path):
+        elif command == "checkforupdates":
+            if not privileges.plugin_privilege_checker(text, command, self.plugin_name):
                 return
             res = update_utils.update_available(message_parse[1])
             if res is True:
@@ -64,10 +68,3 @@ class Plugin(PluginBase):
             else:
                 GS.gui_service.quick_gui(f"There is no update available for: [{message_parse[1]}].",
                                          text_type='header', box_align='left', ignore_whisper=True)
-            return
-
-    def quit(self):
-        dprint("Exiting Auto-Updater Plugin...")
-
-    def get_metadata(self):
-        return self.metadata
