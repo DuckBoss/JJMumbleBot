4b79345522ee25cb02d270c075dfe6519e55bab2
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-07 03:26:01 -0400

    Fixed print statements in config debugs

--- JJMumbleBot/lib/utils/utils.py
+++ JJMumbleBot/lib/utils/utils.py
@@ -1,30 +1,22 @@
-import JJMumbleBot.settings.global_settings as gs
-from time import time
-from datetime import timedelta
-from JJMumbleBot.lib.utils.print_utils import rprint
-
-
-# Calculates the bot service up-time.
-def check_time():
-    gs.seconds = time() - gs.start_seconds
-    return f"Up-time: {str(timedelta(seconds=gs.seconds)).split('.')[0]}"
+from JJMumbleBot.settings.global_settings import cfg, bot_service
+from JJMumbleBot.lib.utils.print_utils import rprint, dprint
 
 
 # Prints all the contents of the config file.
 def debug_config():
-    print("\n-------------------------------------------")
-    print("Debug Configuration File:")
-    for sect in gs.cfg.sections():
-        print(f"[{sect}]")
-        for (key, val) in gs.cfg.items(sect):
-            print(f"{key}={val}")
-    print("-------------------------------------------\n")
+    dprint("\n-------------------------------------------")
+    dprint("Debug Configuration File:")
+    for sect in cfg.sections():
+        dprint(f"[{sect}]")
+        for (key, val) in cfg.items(sect):
+            dprint(f"{key}={val}")
+    dprint("-------------------------------------------\n")
 
 
 # A callback test that prints out the test outputs of all the registered plugins.
 def plugin_callback_test():
     # Plugin Callback Tests
     rprint("######### Running plugin callback tests #########")
-    for plugin in gs.bot_service.bot_plugins.values():
+    for plugin in bot_service.bot_plugins.values():
         plugin.plugin_test()
     rprint("######### Plugin callback tests complete #########")
