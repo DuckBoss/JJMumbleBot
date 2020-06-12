a6a860537867aff124e2bce98505d714855a4e03
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-12 14:38:12 -0400

    Update versions for future release

--- JJMumbleBot/lib/resources/strings.py
+++ JJMumbleBot/lib/resources/strings.py
@@ -85,5 +85,5 @@ P_THREAD_SINGLE = 'UseSingleThread'
 ###########################################################################
 # BOT META INFORMATION STRINGS
 META_NAME = "JJMumbleBot"
-META_VERSION = "3.0.7"
+META_VERSION = "3.0.8"
 ###########################################################################
