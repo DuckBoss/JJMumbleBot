0618d015ac5abd6e363c796fa3c1ad08d7c204bf
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-06-22 01:56:45 -0400

    Added channel/user collection for the web interface

--- JJMumbleBot/lib/monitor_service.py
+++ JJMumbleBot/lib/monitor_service.py
@@ -1,6 +1,7 @@
 import psutil
 import platform
 from JJMumbleBot.settings import global_settings
+from JJMumbleBot.lib.utils.runtime_utils import get_all_channels
 
 
 def get_all_users():
@@ -8,6 +9,29 @@ def get_all_users():
         "users_count": global_settings.mumble_inst.users.count(),
         "all_users": global_settings.mumble_inst.users
     }
+
+
+def get_all_online():
+    online_user_info = {
+        "channels": {},
+        "users": {}
+    }
+    all_channels_dict = get_all_channels()
+
+    users_in_channels = {}
+    for channel_id in all_channels_dict:
+        all_users_in_channel = all_channels_dict[channel_id].get_users()
+        users_in_channels[channel_id] = {}
+        for user in all_users_in_channel:
+            users_in_channels[channel_id][user["name"]] = {
+                f'name': f'{user["name"]}',
+                f'user_id': f'{user["user_id"]}',
+                f'channel_id': f'{user["channel_id"]}'
+            }
+
+    online_user_info["channels"] = all_channels_dict
+    online_user_info["users"] = users_in_channels
+    return online_user_info
 
 
 def get_hardware_info():
