61e2fca66988cfc072bf2dd7a646359e29eb7a21
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-07-18 12:42:24 -0400

    Ported auto-updater plugin to new command system

--- JJMumbleBot/plugins/core/auto_updater/auto_updater.py
+++ JJMumbleBot/plugins/core/auto_updater/auto_updater.py
@@ -1,6 +1,5 @@
 from JJMumbleBot.lib.plugin_template import PluginBase
 from JJMumbleBot.settings import global_settings as GS
-from JJMumbleBot.lib import privileges
 from JJMumbleBot.lib.utils.print_utils import rprint, dprint
 from JJMumbleBot.lib.utils.plugin_utils import PluginUtilityService
 from JJMumbleBot.lib.utils.logging_utils import log
@@ -24,48 +23,40 @@ class Plugin(PluginBase):
         dprint(f"Exiting {self.plugin_name} plugin...", origin=L_SHUTDOWN)
         log(INFO, f"Exiting {self.plugin_name} plugin...", origin=L_SHUTDOWN)
 
-    def get_metadata(self):
-        return self.metadata
+    def cmd_updatedependency(self, data):
+        all_data = data.message.strip().split()
+        res = update_utils.update_available(all_data[1])
+        if res is True:
+            updated_version = update_utils.check_and_update(all_data[1],
+                                                            pip_cmd=self.metadata[C_PLUGIN_SET][P_PIP_CMD])
+            if updated_version:
+                GS.gui_service.quick_gui(f"Dependency: [{all_data[1]}] has been updated to v{updated_version}",
+                                         text_type='header', box_align='left', ignore_whisper=True)
+                log(INFO, f"Dependency: [{all_data[1]}] has been updated to v{updated_version}",
+                    origin=L_DEPENDENCIES)
+                return
+            GS.gui_service.quick_gui(f"Dependency: [{all_data[1]}] could not be updated.",
+                                     text_type='header', box_align='left', ignore_whisper=True)
+            log(INFO, f"Dependency: [{all_data[1]} could not be updated.", origin=L_DEPENDENCIES)
+        elif res is None:
+            GS.gui_service.quick_gui(f"The package: [{all_data[1]}] is not a dependency of this software.",
+                                     text_type='header', box_align='left', ignore_whisper=True)
+            return
+        else:
+            GS.gui_service.quick_gui(f"There is no update available for: [{all_data[1]}].",
+                                     text_type='header', box_align='left', ignore_whisper=True)
 
-    def process(self, text):
-        message = text.message.strip()
-        message_parse = message[1:].split(' ', 1)
-        command = message_parse[0]
-
-        if command == "updatedependency":
-            if not privileges.plugin_privilege_checker(text, command, self.plugin_name):
-                return
-            res = update_utils.update_available(message_parse[1])
-            if res is True:
-                updated_version = update_utils.check_and_update(message_parse[1], pip_cmd=self.metadata[C_PLUGIN_SET][P_PIP_CMD])
-                if updated_version:
-                    GS.gui_service.quick_gui(f"Dependency: [{message_parse[1]}] has been updated to v{updated_version}",
-                                             text_type='header', box_align='left', ignore_whisper=True)
-                    log(INFO, f"Dependency: [{message_parse[1]}] has been updated to v{updated_version}", origin=L_DEPENDENCIES)
-                    return
-                GS.gui_service.quick_gui(f"Dependency: [{message_parse[1]}] could not be updated.",
-                                         text_type='header', box_align='left', ignore_whisper=True)
-                log(INFO, f"Dependency: [{message_parse[1]} could not be updated.", origin=L_DEPENDENCIES)
-            elif res is None:
-                GS.gui_service.quick_gui(f"The package: [{message_parse[1]}] is not a dependency of this software.",
-                                         text_type='header', box_align='left', ignore_whisper=True)
-                return
-            else:
-                GS.gui_service.quick_gui(f"There is no update available for: [{message_parse[1]}].",
-                                         text_type='header', box_align='left', ignore_whisper=True)
-
-        elif command == "checkforupdates":
-            if not privileges.plugin_privilege_checker(text, command, self.plugin_name):
-                return
-            if len(message_parse) < 2:
-                return
-            res = update_utils.update_available(message_parse[1])
-            if res is True:
-                GS.gui_service.quick_gui(f"There is a newer version of: [{message_parse[1]}] available.",
-                                         text_type='header', box_align='left', ignore_whisper=True)
-            elif res is None:
-                GS.gui_service.quick_gui(f"The package: [{message_parse[1]}] is not a dependency of this software.",
-                                         text_type='header', box_align='left', ignore_whisper=True)
-            else:
-                GS.gui_service.quick_gui(f"There is no update available for: [{message_parse[1]}].",
-                                         text_type='header', box_align='left', ignore_whisper=True)
+    def cmd_checkforupdates(self, data):
+        all_data = data.message.strip().split()
+        if len(all_data) < 2:
+            return
+        res = update_utils.update_available(all_data[1])
+        if res is True:
+            GS.gui_service.quick_gui(f"There is a newer version of: [{all_data[1]}] available.",
+                                     text_type='header', box_align='left', ignore_whisper=True)
+        elif res is None:
+            GS.gui_service.quick_gui(f"The package: [{all_data[1]}] is not a dependency of this software.",
+                                     text_type='header', box_align='left', ignore_whisper=True)
+        else:
+            GS.gui_service.quick_gui(f"There is no update available for: [{all_data[1]}].",
+                                     text_type='header', box_align='left', ignore_whisper=True)
