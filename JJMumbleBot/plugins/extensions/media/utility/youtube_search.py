e2ec1ed0a627fb9382f349c38521b301506135ff
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-12-12 19:17:36 -0500

    Fixed ytsearch command - #298

--- JJMumbleBot/plugins/extensions/media/utility/youtube_search.py
+++ JJMumbleBot/plugins/extensions/media/utility/youtube_search.py
@@ -42,7 +42,7 @@ class YoutubeSearch:
         BASE_URL = "https://youtube.com"
         url = f"{BASE_URL}/results?search_query={encoded_search}"
         response = requests.get(url).text
-        while 'window["ytInitialData"]' not in response:
+        while "ytInitialData" not in response:
             response = requests.get(url).text
         results = self.parse_html(response)
         if self.max_results is not None and len(results) > self.max_results:
@@ -51,27 +51,19 @@ class YoutubeSearch:
 
     def parse_html(self, response):
         results = []
-        start = (
-            response.index('window["ytInitialData"]')
-            + len('window["ytInitialData"]')
-            + 3
-        )
+        start = (response.index("ytInitialData") + len("ytInitialData") + 3)
         end = response.index("};", start) + 1
         json_str = response[start:end]
         data = json.loads(json_str)
 
-        videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
-            "sectionListRenderer"
-        ]["contents"][0]["itemSectionRenderer"]["contents"]
+        videos = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"]["contents"]
 
         for video in videos:
             res = {}
             if "videoRenderer" in video.keys():
                 video_data = video["videoRenderer"]
                 res["title"] = video_data["title"]["runs"][0]["text"]
-                res["href"] = video_data["navigationEndpoint"]["commandMetadata"][
-                    "webCommandMetadata"
-                ]["url"]
+                res["href"] = video_data["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"]["url"]
                 results.append(res)
         return results
 
