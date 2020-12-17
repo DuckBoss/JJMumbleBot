4661cd4dc8dc2af3dd0ab0a10e225366ff1a7694
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-12-17 00:11:42 -0500

    Updated logging_utils to support error displaying and message printing.

--- JJMumbleBot/lib/utils/logging_utils.py
+++ JJMumbleBot/lib/utils/logging_utils.py
@@ -1,6 +1,7 @@
 from JJMumbleBot.settings import global_settings
 from JJMumbleBot.settings import runtime_settings
 from JJMumbleBot.lib.resources.strings import INFO, DEBUG, WARNING, CRITICAL, META_NAME, META_VERSION, L_GENERAL, C_LOGGING, P_LOG_DIR
+from JJMumbleBot.lib.utils.print_utils import rprint, dprint, PrintMode
 import logging
 
 
@@ -20,16 +21,43 @@ def initialize_logging():
     global_settings.log_service.addHandler(handler)
 
 
-def log(level, message, origin=None):
+def display_error(gui_service, message: str):
+    gui_service.quick_gui(
+        message,
+        text_type='header',
+        text_align='left',
+        box_align='left'
+    )
+
+
+def log(level: str, message: str, origin: str = None, error_type: str = None, gui_service=None, print_mode: int = -1):
+    # Don't attempt to log anything if the use_logging flag is false.
     if not runtime_settings.use_logging:
         return
-    if not global_settings.log_service:
-        return
+    # If logging is enabled and the log service is missing, raise an error.
+    if global_settings.log_service is None:
+        from JJMumbleBot.lib.errors import LogError
+        raise LogError("ERROR: Logging is enabled but an instance of the logging service could not be created!")
+    # Log the message based on the event level.
     if level == INFO:
-        global_settings.log_service.info(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{message}')
+        global_settings.log_service.info(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
+                                         f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
     elif level == DEBUG:
-        global_settings.log_service.debug(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{message}')
+        global_settings.log_service.debug(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
+                                          f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
     elif level == WARNING:
-        global_settings.log_service.warning(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{message}')
+        global_settings.log_service.warning(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
+                                            f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
     elif level == CRITICAL:
-        global_settings.log_service.critical(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:{message}')
+        global_settings.log_service.critical(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
+                                             f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+    # Additionally, print out the log message if required.
+    if print_mode == PrintMode.REG_PRINT.value:
+        rprint(message, error_type=error_type)
+    elif print_mode == PrintMode.DEBUG_PRINT.value:
+        dprint(message, error_type=error_type)
+    # Display the error in the mumble channel chat if required.
+    if error_type is not None and gui_service is not None:
+        display_error(gui_service, message)
+
+
