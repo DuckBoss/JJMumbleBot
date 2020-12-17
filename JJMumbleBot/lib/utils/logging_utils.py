3deb71840911c0ccc17f81ec7c7697e0a757fb4e
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-12-17 17:46:14 -0500

    Updated logging utils to support multi-line messages

--- JJMumbleBot/lib/utils/logging_utils.py
+++ JJMumbleBot/lib/utils/logging_utils.py
@@ -1,3 +1,4 @@
+from typing import List, Union
 from JJMumbleBot.settings import global_settings
 from JJMumbleBot.settings import runtime_settings
 from JJMumbleBot.lib.resources.strings import INFO, DEBUG, WARNING, ERROR, CRITICAL, META_NAME, META_VERSION, L_GENERAL, C_LOGGING, P_LOG_DIR
@@ -30,7 +31,7 @@ def display_error(gui_service, message: str):
     )
 
 
-def log(level: str, message: str, origin: str = None, error_type: str = None, gui_service=None, print_mode: int = -1):
+def log(level: str, message: Union[List[str], str], origin: str = None, error_type: str = None, gui_service=None, print_mode: int = -1):
     # Don't attempt to log anything if the use_logging flag is false.
     if not runtime_settings.use_logging:
         return
@@ -38,29 +39,34 @@ def log(level: str, message: str, origin: str = None, error_type: str = None, gu
     if global_settings.log_service is None:
         from JJMumbleBot.lib.errors import LogError
         raise LogError("ERROR: Logging is enabled but an instance of the logging service could not be created!")
+    # If the provided message is not a list, convert it to a list.
+    if not isinstance(message, list):
+        message = [message]
     # Log the message based on the event level.
+    log_msg = "\n".join(message) if len(message) > 1 else message[0]
     if level == INFO:
         global_settings.log_service.info(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
-                                         f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+                                         f'{"<"+error_type+">:" if error_type is not None else ""}{log_msg}')
     elif level == DEBUG:
         global_settings.log_service.debug(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
-                                          f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+                                          f'{"<"+error_type+">:" if error_type is not None else ""}{log_msg}')
     elif level == WARNING:
         global_settings.log_service.warning(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
-                                            f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+                                            f'{"<"+error_type+">:" if error_type is not None else ""}{log_msg}')
     elif level == ERROR:
         global_settings.log_service.error(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
-                                            f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+                                            f'{"<"+error_type+">:" if error_type is not None else ""}{log_msg}')
     elif level == CRITICAL:
         global_settings.log_service.critical(f'[{META_NAME}({META_VERSION}).{origin if origin is not None else L_GENERAL}]:'
-                                             f'{"<"+error_type+">:" if error_type is not None else ""}{message}')
+                                             f'{"<"+error_type+">:" if error_type is not None else ""}{log_msg}')
     # Additionally, print out the log message if required.
     if print_mode == PrintMode.REG_PRINT.value:
-        rprint(message, origin=origin, error_type=error_type)
+        rprint(log_msg, origin=origin, error_type=error_type)
     elif print_mode == PrintMode.VERBOSE_PRINT.value:
-        dprint(message, origin=origin, error_type=error_type)
+        dprint(log_msg, origin=origin, error_type=error_type)
     # Display the error in the mumble channel chat if required.
     if error_type is not None and gui_service is not None:
-        display_error(gui_service, message)
+        print_msg = "<br>".join(message) if len(message) > 1 else message[0]
+        display_error(gui_service, print_msg)
 
 
