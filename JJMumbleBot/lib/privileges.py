9db106e03b5ceff49ca6f9c445c55fa2c8efe411
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-12-19 00:53:47 -0500

    Updated privilege system with new logging features

--- JJMumbleBot/lib/privileges.py
+++ JJMumbleBot/lib/privileges.py
@@ -4,8 +4,8 @@ from JJMumbleBot.lib.resources.strings import *
 from JJMumbleBot.lib.utils.database_management_utils import get_memory_db
 from JJMumbleBot.lib.utils.database_utils import GetDB, InsertDB, UpdateDB
 from JJMumbleBot.lib.utils.logging_utils import log
-from JJMumbleBot.lib.utils.print_utils import dprint, rprint
-from JJMumbleBot.settings import global_settings as GS
+from JJMumbleBot.lib.utils.print_utils import PrintMode
+from JJMumbleBot.settings import global_settings as gs
 
 
 class Privileges(Enum):
@@ -19,12 +19,10 @@ class Privileges(Enum):
 
 def privileges_check(user):
     # Print and log a critical database access error if the database has not been initialized.
-    if not GS.mumble_db_string:
-        dprint(
-            f"The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!\nTrying to initialize database...")
+    if not gs.mumble_db_string:
         log(CRITICAL,
-            f"The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!\nTrying to initalize database...",
-            origin=L_DATABASE)
+            "The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!",
+            origin=L_DATABASE, error_type=GEN_PROCESS_ERR, print_mode=PrintMode.VERBOSE_PRINT.value)
         return -1
     # Retrieve the user information in the database.
     user_data = GetDB.get_user_data(db_cursor=get_memory_db().cursor(), user_name=user['name'])
@@ -42,12 +40,10 @@ def privileges_check(user):
 
 
 def plugin_privileges_check(command, plugin_name):
-    if not GS.mumble_db_string:
-        dprint(
-            f"The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!\nTrying to initialize database...")
+    if not gs.mumble_db_string:
         log(CRITICAL,
-            f"The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!\nTrying to initalize database...",
-            origin=L_DATABASE)
+            f"The JJMumbleBot database has not been initialized, but a user privilege check is trying to access it!",
+            origin=L_DATABASE, error_type=GEN_PROCESS_ERR, print_mode=PrintMode.VERBOSE_PRINT.value)
         return -1
     # Retrieve the command information in the database.
     command_data = GetDB.get_plugin_data(db_cursor=get_memory_db().cursor(), plugin_name=plugin_name)
@@ -61,32 +57,30 @@ def plugin_privileges_check(command, plugin_name):
 
 
 def plugin_privilege_checker(text, command, plugin_name):
-    if not privileges_check(GS.mumble_inst.users[text.actor]) >= plugin_privileges_check(command, plugin_name):
-        rprint(
-            f"User [{GS.mumble_inst.users[text.actor]['name']}] does not have the user privileges to use this command: [{command}]")
-        GS.gui_service.quick_gui(f"User [{GS.mumble_inst.users[text.actor]['name']}] does not have the user "
+    if not privileges_check(gs.mumble_inst.users[text.actor]) >= plugin_privileges_check(command, plugin_name):
+        log(WARNING,
+            f"User [{gs.mumble_inst.users[text.actor]['name']}] does not have the user privileges to use this command: [{command}]",
+            origin=L_USER_PRIV, error_type=GEN_PROCESS_WARN, print_mode=PrintMode.VERBOSE_PRINT.value)
+        gs.gui_service.quick_gui(f"User [{gs.mumble_inst.users[text.actor]['name']}] does not have the user "
                                  f"privileges to use this command: [{command}]", text_type='header', box_align='left')
-        log(INFO,
-            f"User [{GS.mumble_inst.users[text.actor]['name']}] does not have the user privileges to use this command: [{command}]",
-            L_COMMAND)
         return False
     return True
 
 
 def get_all_privileges():
-    priv_text = f"<font color='{GS.cfg[C_PGUI_SETTINGS][P_TXT_HEAD_COL]}'>All User Privileges:</font>"
+    priv_text = f"<font color='{gs.cfg[C_PGUI_SETTINGS][P_TXT_HEAD_COL]}'>All User Privileges:</font>"
     for i, user in enumerate(GetDB.get_all_user_data(get_memory_db().cursor())):
-        priv_text += f"<br><font color='{GS.cfg[C_PGUI_SETTINGS][P_TXT_IND_COL]}'>[{user[0]}]</font> - {user[1]}"
+        priv_text += f"<br><font color='{gs.cfg[C_PGUI_SETTINGS][P_TXT_IND_COL]}'>[{user[0]}]</font> - {user[1]}"
     return priv_text
 
 
 def get_blacklist():
-    blklist_txt = f"<font color='{GS.cfg[C_PGUI_SETTINGS][P_TXT_HEAD_COL]}'>Blacklisted Users:</font>"
+    blklist_txt = f"<font color='{gs.cfg[C_PGUI_SETTINGS][P_TXT_HEAD_COL]}'>Blacklisted Users:</font>"
     counter = 0
     all_user_data = GetDB.get_all_user_data(get_memory_db().cursor())
     for i, user in enumerate(all_user_data):
         if int(user[1]) == int(Privileges.BLACKLIST.value):
-            blklist_txt += f"<br><font color='{GS.cfg[C_PGUI_SETTINGS][P_TXT_IND_COL]}'>[{counter}]</font> - {user[0]}"
+            blklist_txt += f"<br><font color='{gs.cfg[C_PGUI_SETTINGS][P_TXT_IND_COL]}'>[{counter}]</font> - {user[0]}"
             counter += 1
     if counter == 0:
         blklist_txt += " The blacklist is empty!"
@@ -99,8 +93,9 @@ def add_to_blacklist(username):
     if username in user_names_list:
         for user in all_user_data:
             if user[0] == username and user[1] == Privileges.BLACKLIST.value:
-                rprint(f"The user: {username} is already in the blacklist.", origin=L_USER_PRIV)
-                log(INFO, f"The user: {username} is already in the blacklist.", origin=L_USER_PRIV)
+                log(WARNING,
+                    f"Could not add the user: {username} to the blacklist since the user is already in the blacklist.",
+                    origin=L_USER_PRIV, error_type=GEN_PROCESS_WARN, print_mode=PrintMode.VERBOSE_PRINT.value)
                 return False
         if UpdateDB.update_user_privileges(db_conn=get_memory_db(), user_name=username, level=int(Privileges.BLACKLIST.value)):
             return True
@@ -124,19 +119,14 @@ def set_privileges(username, level, sender):
     if username in user_names_list:
         for user in all_user_data:
             if user[0] == username and username == sender:
-                dprint(f"This user: [{username}] tried to modify their own user privileges. Modification denied.",
-                       origin=L_USER_PRIV)
                 log(WARNING,
                     f"This user: [{username}] tried to modify their own user privileges, the modification was denied.",
-                    origin=L_USER_PRIV)
+                    origin=L_USER_PRIV, error_type=GEN_PROCESS_WARN, print_mode=PrintMode.VERBOSE_PRINT.value)
                 return False
             if user[0] == username and privileges_check(sender) <= user[1]:
-                dprint(
-                    f"This user: [{sender['name']}] tried to modify privileges for a user with equal/higher privileges: [{username}]",
-                    origin=L_USER_PRIV)
                 log(WARNING,
                     f"This user: [{sender['name']}] tried to modify privileges for a user with equal/higher privileges: [{username}]",
-                    origin=L_USER_PRIV)
+                    origin=L_USER_PRIV, error_type=GEN_PROCESS_WARN, print_mode=PrintMode.VERBOSE_PRINT.value)
                 return False
     if UpdateDB.update_user_privileges(db_conn=get_memory_db(), user_name=username, level=int(level)):
         return True
@@ -147,5 +137,8 @@ def add_to_privileges(username, level):
     if InsertDB.insert_new_user(db_conn=get_memory_db(), username=username):
         if InsertDB.insert_new_permission(db_conn=get_memory_db(), username=username,
                                           permission_level=int(level)):
+            log(INFO,
+                f"A new user: [{username}] has been added to the user privileges.",
+                origin=L_USER_PRIV, print_mode=PrintMode.VERBOSE_PRINT.value)
             return True
     return False
