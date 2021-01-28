d01ea9ed0649db9711fd285353c1d9877c3f79d2
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2021-01-28 02:52:13 -0500

    Updated metadatas to v5.0.0 instead of v4.0.0

--- JJMumbleBot/lib/resources/strings.py
+++ JJMumbleBot/lib/resources/strings.py
@@ -121,5 +121,5 @@ P_THREAD_SINGLE = 'UseSingleThread'
 ###########################################################################
 # BOT META INFORMATION STRINGS
 META_NAME = "JJMumbleBot"
-META_VERSION = "4.4.0"
+META_VERSION = "5.0.0"
 ###########################################################################
