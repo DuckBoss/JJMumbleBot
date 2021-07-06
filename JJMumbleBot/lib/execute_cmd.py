fa9884828baa63d977debd1aad8242096da195e5
Jason Jerome <jasonjerome16@gmail.com>
2021-07-06 14:28:38 -0400

    Updated command privilege denial message
    - Blacklisted users now receive a different denial message than other users that lack command privileges.

--- JJMumbleBot/lib/execute_cmd.py
+++ JJMumbleBot/lib/execute_cmd.py
@@ -1,4 +1,4 @@
-from JJMumbleBot.lib import privileges
+from JJMumbleBot.lib.privileges import Privileges, privileges_check,  plugin_privilege_checker
 from JJMumbleBot.lib.resources.strings import P_THREAD_WAIT, P_THREAD_SINGLE, C_PLUGIN_SETTINGS, WARNING
 from JJMumbleBot.settings import global_settings
 from JJMumbleBot.lib.utils.logging_utils import log
@@ -39,11 +39,19 @@ def execute_command(com):
         return
 
     # Check user privileges before attempting to execute any commands.
-    if not privileges.plugin_privilege_checker(com.text, com.command, plugin.plugin_name):
+    if privileges_check(com.text.actor) > Privileges.BLACKLIST.value:
+        if not plugin_privilege_checker(com.text, com.command, plugin.plugin_name):
+            global_settings.gui_service.quick_gui(
+                f"{com.text.actor['name']} does not have the user privileges to use <code>{get_command_token()}{com.command}</code>.",
+                text_type='header', box_align='left')
+            log(INFO, f"{com.text.actor['name']} tried to use the {get_command_token()}{com.command} command, but lacked the user privileges to do so.",
+            return
+    else:
         global_settings.gui_service.quick_gui(
-            f"{com.text.actor['name']} does not have the user privileges to use <code>{get_command_token()}{com.command}</code>.",
-            text_type='header', box_align='left')
-        log(INFO, f"{com.text.actor['name']} tried to use the {get_command_token()}{com.command} command, but lacked the user privileges to do so.",
+            f"Blacklisted user [{com.text.actor['name']}] does not have the user "
+            f"privileges to use this command: [{com.command}]", text_type='header', box_align='left')
+        log(INFO,
+            f"{com.text.actor['name']} tried to use the {get_command_token()}{com.command} command, but is currently blacklisted.",
             origin=L_COMMAND, print_mode=PrintMode.VERBOSE_PRINT.value)
         return
 
