f097636158a17e001b6057f504fed82ff38b6aa7
Jason Jerome <jasonjerome16@gmail.com>
2021-07-07 15:38:33 -0400

    Updated core_commands plugin unit tests

--- JJMumbleBot/plugins/core/core_commands/tests/test_corecommands_metadata.py
+++ JJMumbleBot/plugins/core/core_commands/tests/test_corecommands_metadata.py
@@ -16,7 +16,7 @@ class TestCoreCommands:
 
     def test_commands_list_size(self):
         commands_list = list(loads(self.cfg[C_PLUGIN_INFO][P_PLUGIN_CMDS]))
-        assert len(commands_list) == 26
+        assert len(commands_list) == 28
 
     def test_match_commands_to_methods(self):
         method_list = [item for item in dir(Plugin) if callable(getattr(Plugin, item)) and item.startswith("cmd_")]
