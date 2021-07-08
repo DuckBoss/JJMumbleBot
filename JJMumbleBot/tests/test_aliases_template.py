f59ba2e0fbcc35c3b6ec9ae7c0a98a8f063267ea
Jason Jerome <jasonjerome16@gmail.com>
2021-07-08 18:28:37 -0400

    Updated unit tests

--- JJMumbleBot/tests/test_aliases_template.py
+++ JJMumbleBot/tests/test_aliases_template.py
@@ -5,15 +5,14 @@ from JJMumbleBot.lib.utils.dir_utils import get_main_dir
 class TestAliasesTemplate:
     def setup_method(self):
         self.alias_file_name = f"{get_main_dir()}/templates/aliases_template.csv"
-        self.aliases_dict = {}
+        self.alias_header_list = []
         try:
             with open(self.alias_file_name, mode='r') as csv_file:
                 csvr = DictReader(csv_file)
-                for i, row in enumerate(csvr):
-                    self.aliases_dict[row['alias'].strip()] = row['command'].strip()
+                self.alias_header_list = list(csvr)[0]
         except IOError:
             print(f"Encountered an IO error reading the alias file: [{self.alias_file_name}] during unit tests.")
 
     def test_alias_fields(self):
-        assert self.aliases_dict["alias"] == "command"
+        assert set(self.alias_header_list) == {"alias", "command"}
 
--- JJMumbleBot/tests/test_permissions_template.py
+++ JJMumbleBot/tests/test_permissions_template.py
@@ -5,15 +5,14 @@ from JJMumbleBot.lib.utils.dir_utils import get_main_dir
 class TestPermissionTemplate:
     def setup_method(self):
         self.permissions_file_name = f"{get_main_dir()}/templates/permissions_template.csv"
-        self.aliases_dict = {}
+        self.permissions_header_list = {}
         try:
             with open(self.permissions_file_name, mode='r') as csv_file:
                 csvr = DictReader(csv_file)
-                for i, row in enumerate(csvr):
-                    self.aliases_dict[row['command'].strip()] = row['level'].strip()
+                self.permissions_header_list = list(csvr)[0]
         except IOError:
             print(f"Encountered an IO error reading the command permissions file: [{self.permissions_file_name}] during unit tests.")
 
-    def test_alias_fields(self):
-        assert self.aliases_dict["command"] == "level"
+    def test_permissions_fields(self):
+        assert set(self.permissions_header_list) == {"command", "level"}
 
